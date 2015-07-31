4b6766fbcd9e16b51f0e756ceaa93ffb540427a5
Archer Lin <twbcs7@gmail.com>
2015-07-31 16:26:20 +0800

    complete front tours index view

--- app/helpers/tours_helper.rb
+++ app/helpers/tours_helper.rb
@@ -13,4 +13,18 @@ module ToursHelper
       "行程規劃 / 五日行程(以上)"
     end
   end
+  def tour_nav(tour)
+    case tour
+    when 1
+      "1日自由行"
+    when 2
+      "2日自由行"
+    when 3
+      "3日自由行"
+    when 4
+      "4日自由行"
+    when 5
+      "5日自由行(以上)"
+    end
+  end
 end
